543653b993e6393cee2df2b19d9f6f44418e7f49
moe shak <moeshak2051@gmail.com>
2019-03-12 21:34:34 -0400

    Update8

--- src/algorithm/UnitTestSorting.java
+++ src/algorithm/UnitTestSorting.java
@@ -23,6 +23,46 @@ public class UnitTestSorting {
 
         //Now implement Unit test for rest of the soring algorithm...................below
 
+        //Bubble sort Unit test
+        try{
+            int[] actual = sort.bubbleSort(unSortedArray);
+            System.out.println("Test passed: Quick Sort is sorted");
+        }catch (AssertionError ex) {
+            System.out.println("Test failed: Quick Sort is not sorted");
+        }
+
+        //Bucket Sort Unit test
+        try{
+            int[] actual = sort.bucketSort(unSortedArray, 10);
+            System.out.println("Test Passed: Bucket Sort is sorted");
+        }catch (AssertionError ex){
+            System.out.println("Test failed: Bucket Sort is not sorted");
+        }
+
+        //Heap Sort Unit test
+        try{
+            int[] actual = sort.heapSort(unSortedArray);
+            System.out.println("Test passed: Heap Sort is sorted");
+        }catch (AssertionError ex){
+            System.out.println("test failed: Heap Sort is not sorted");
+        }
+
+        //Merge Sort Unit test
+        try{
+            int[] actual = sort.quickSort(unSortedArray, 0 , unSortedArray.length-1);
+            System.out.println("Test passed: Merge Sort is sorted");
+        }catch (AssertionError ex){
+            System.out.println("Test failed: Merge Sort is not sorted");
+        }
+
+        //Insertion Sort Unit test
+        try{
+            int[] actual = sort.insertionSort(unSortedArray);
+            System.out.println("Test passed: Insertion Sort is sorted");
+        }catch (AssertionError ex){
+            System.out.println("Test failed: Insertion Sort is not sorted");
+        }
+
 
 
 
